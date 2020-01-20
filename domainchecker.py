ba645ac137ccd142c314a481bf4c74dd5fd0d307
CodeByZen <1809483+codebyzen@users.noreply.github.com>
2020-01-20 17:31:28 +0300

    Add [crt.sh](https://crt.sh) service

--- domainchecker.py
+++ domainchecker.py
@@ -7,6 +7,7 @@ from urllib.parse import urlparse
 import urllib.request
 import ssl
 from multiprocessing.dummy import Pool as ThreadPool
+import json
 
 def getFile(filename):
 	try:
@@ -32,6 +33,7 @@ parser = argparse.ArgumentParser(
  ''')
 parser.add_argument('-l', dest="list", type=getFile, help='domain list file (each domain from new line)') 
 parser.add_argument('-d', dest="domains", type=parse_inline_domains, help='domain list string (separates by ",")') 
+parser.add_argument('-crt', dest="crt", action='store_true', help='get additional domains from crt.sh') 
 parser.add_argument('-c', dest="codes", action='store_true', help='return [return code] domain name') 
 parser.add_argument('-g', dest="good", action='store_true', help='return only good domains') 
 parser.add_argument('-o', dest="output", type=argparse.FileType('w'), help='file to write result') 
@@ -40,7 +42,63 @@ if len(sys.argv)<=1:
     parser.print_usage(sys.stderr)
     sys.exit(1)
 args = parser.parse_args()
-print(args)
+
+
+def getDataFromCRT(domain):
+	ctx = ssl.create_default_context()
+	ctx.check_hostname = False
+	ctx.verify_mode = ssl.CERT_NONE
+	ctx.ua = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
+
+	domainname = domain.strip()
+	matches = re.search("http[s]?:\/\/", domainname)
+	if matches!=None:
+		purl = urlparse(domainname)
+		domainname=purl.netloc
+
+	data = urllib.parse.urlencode({'q': domainname, 'output': 'json'})
+	data = data.encode('ascii')
+	
+	ret = False
+	try:
+		with urllib.request.urlopen("https://crt.sh", data, context=ctx) as f:
+			raw = f.read().decode('utf-8')
+	except urllib.error.HTTPError as e:
+		ret = e.code
+	except urllib.error.URLError as e:
+		if hasattr(e, 'reason'):
+			ret = e.reason.errno
+		elif hasattr(e, 'code'):
+			ret = e.code
+		else:
+			ret = 'unknown error'
+	except ConnectionResetError as e:
+		ret = e
+	
+	if (ret):
+		print(ret)
+		exit(-1)
+
+	output = json.loads(raw)
+	urlsarray = []
+	for i in output:
+		if i['name_value']:
+			url = i['name_value'].split("\n")
+			# url = i['name_value']
+			urlsarray = urlsarray+url
+	
+	clean_domains = []
+	for i in urlsarray:
+		if (not re.match(".*@.*|\*\.",i)):
+			clean_domains.append(i)	
+
+	# insert the list to the set 
+	list_set = set(clean_domains) 
+	# convert the set to the list 
+	clean_domains = (list(list_set))	
+
+	return clean_domains
+
 
 def cleanlist(dList):
 	dpList = []
@@ -96,8 +154,13 @@ if (args.list == None):
 else:
 	domains = args.list
 
-domains = cleanlist(domains)
+if args.crt:
+	allDomains = []
+	for i in domains:
+		allDomains = allDomains+getDataFromCRT(i)
+	domains = allDomains
 
+domains = cleanlist(domains)
 
 pool = ThreadPool(8)
 results = pool.map(tryConnect, domains)
